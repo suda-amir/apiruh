a4bd525841cbd33c81380b3835884949d5ae75f3
Janakiram <ajanakiram.amirineni@gmail.com>
2018-01-28 17:43:25 +0530

    apiruh-new

--- apiruh.sql
+++ apiruh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 27, 2018 at 07:05 PM
+-- Generation Time: Jan 28, 2018 at 01:13 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -34,89 +34,6 @@ CREATE TABLE `client` (
   `client_addr` text NOT NULL,
   `client_poc` text NOT NULL,
   `client_poc_no` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=ascii;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employee`
---
-
-CREATE TABLE `employee` (
-  `empid` varchar(15) DEFAULT NULL,
-  `name` varchar(500) DEFAULT NULL,
-  `nat_emp` varchar(100) DEFAULT NULL,
-  `personal_no` varchar(50) DEFAULT NULL,
-  `dob` varchar(50) DEFAULT NULL,
-  `doj` varchar(50) DEFAULT NULL,
-  `design` varchar(200) DEFAULT NULL,
-  `pan_no` varchar(10) DEFAULT NULL,
-  `aadhar_no` int(12) DEFAULT NULL,
-  `perm_addr` varchar(5000) DEFAULT NULL,
-  `contct_det` varchar(100) DEFAULT NULL,
-  `emr_cntct_per` varchar(200) DEFAULT NULL,
-  `emr_cntct_no` varchar(100) DEFAULT NULL,
-  `blood_grp` varchar(100) DEFAULT NULL,
-  `qualifications` varchar(500) DEFAULT NULL,
-  `supprt_docs` varchar(1000) DEFAULT NULL,
-  `serv_agr` varchar(500) DEFAULT NULL,
-  `offr_letter` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=ascii;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `emp_leaves`
---
-
-CREATE TABLE `emp_leaves` (
-  `empid` text NOT NULL,
-  `paid_leaves` int(3) NOT NULL,
-  `unpaid_leaves` int(3) NOT NULL,
-  `earned_leaves` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=ascii;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `holidays`
---
-
-CREATE TABLE `holidays` (
-  `public` text NOT NULL,
-  `admin_defined` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=ascii;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `privileges`
---
-
-CREATE TABLE `privileges` (
-  `empid` text NOT NULL,
-  `priv` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=ascii;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `status_report`
---
-
-CREATE TABLE `status_report` (
-  `status_id` text NOT NULL,
-  `emp_id` text NOT NULL,
-  `client_id` text NOT NULL,
-  `status_date` text NOT NULL,
-  `start_time` text NOT NULL,
-  `end_time` text NOT NULL,
-  `work_hrs` int(3) NOT NULL,
-  `work_status` text NOT NULL,
-  `nat_work` text NOT NULL,
-  `work_subject` text NOT NULL,
-  `billed` text NOT NULL,
-  `add_comments` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 COMMIT;
 
