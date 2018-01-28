ae98a6c44f98c7caaf70361b6117a4fa13005624
Sudarshan Amirineni <suda_amir@hotmail.com>
2018-01-28 19:03:38 +0530

    Config Files

--- admin_1/html/admin/config.php
+++ admin_1/html/admin/config.php
@@ -8,11 +8,7 @@ $databaseName='apiruh';
 
 			try {
 			    $connection = new PDO("mysql:host=$databaseServer;dbname=$databaseName", $databaseUsername, $databasePassword);
-			    // set the PDO error mode to exception
 			    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			    //echo "Connected successfully";
-			    
-
 		    }
 			catch(PDOException $e)
 			    {
