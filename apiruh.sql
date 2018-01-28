8f22e8b1048f4fb1c1500d89805aea75e93c5a55
Janakiram <ajanakiram.amirineni@gmail.com>
2018-01-28 21:18:16 +0530

    db-update

--- apiruh.sql
+++ apiruh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 28, 2018 at 02:28 PM
+-- Generation Time: Jan 28, 2018 at 04:47 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
