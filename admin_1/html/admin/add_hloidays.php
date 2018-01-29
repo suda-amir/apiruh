c51547343f4c22ea23fe9a31d41fc05c6135ec80
Janakiram <ajanakiram.amirineni@gmail.com>
2018-01-29 07:35:02 +0530

    holidays-query-only

--- admin_1/html/admin/add_hloidays.php
+++ admin_1/html/admin/add_hloidays.php
@@ -8,7 +8,7 @@ $start_date = $_POST['start'];
 $end_date = $_POST['end'];
 $reason = $_POST['reason'];
 try{
-	$stmt = $connection->prepare("INSERT INTO `holidays`(`hol_type`, `hol_date`, `hol_desc`, `hol_start`, `hol_end`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5])");
+	$stmt = $connection->prepare("INSERT INTO `holidays`(`hol_type`, `hol_date`, `hol_desc`, `hol_start`, `hol_end`) VALUES (:title,:start_date,:end_date,[value-4],[value-5])");
 	
 	$stmt->bindParam("title", $title,PDO::PARAM_STR) ;
 	$stmt->bindParam("start_date", $start_date,PDO::PARAM_STR) ;
