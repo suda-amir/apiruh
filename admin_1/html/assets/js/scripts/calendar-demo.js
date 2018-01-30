f4de987c43fd7501280058e758f5327c5d5c2faf
Sudarshan Amirineni <suda_amir@hotmail.com>
2018-01-30 17:32:15 +0530

    Events Called in Calender page

--- admin_1/html/assets/js/scripts/calendar-demo.js
+++ admin_1/html/assets/js/scripts/calendar-demo.js
@@ -12,6 +12,10 @@
         this.$saveCategoryBtn = $('.save-category'),
         this.$calendarObj = null
     };
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
 
     // handler for clicking on an empty calendar field
     CalendarApp.prototype.onSelect = function (start, end, allDay) {
@@ -124,6 +128,7 @@
             eventObj.remove();
         }
     },
+
     
     // initialize the external events
     CalendarApp.prototype.enableDrag = function() {
@@ -170,6 +175,52 @@
             eventRender: function(event, element, view) {
                 event.allDay = event.allDay == true ? true : false;
             },
+            events: [
+            {
+                title: 'All Day Event',
+                start: new Date(y, m, 1)
+            },
+            {
+                title: 'Long Event',
+                start: new Date(y, m, d-5),
+                end: new Date(y, m, d-2)
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: new Date(y, m, d-3, 16, 0),
+                allDay: false
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: new Date(y, m, d+4, 16, 0),
+                allDay: false
+            },
+            {
+                title: 'Meeting',
+                start: new Date(y, m, d, 10, 30),
+                allDay: false
+            },
+            {
+                title: 'Lunch',
+                start: new Date(y, m, d, 12, 0),
+                end: new Date(y, m, d, 14, 0),
+                allDay: false
+            },
+            {
+                title: 'Birthday Party',
+                start: new Date(y, m, d+1, 19, 0),
+                end: new Date(y, m, d+1, 22, 30),
+                allDay: false
+            },
+            {
+                title: 'Click for Google',
+                start: new Date(y, m, 28),
+                end: new Date(y, m, 29),
+                url: 'http://google.com/'
+            }
+        ]
         });
     }
     
