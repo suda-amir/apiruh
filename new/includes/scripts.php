1928dd3d721aae959a12fe7a6550432579f2f4f5
Janakiram <ajanakiram.amirineni@gmail.com>
2018-03-09 01:21:51 +0530

    status reports update

--- new/includes/scripts.php
+++ new/includes/scripts.php
@@ -8,11 +8,7 @@
     <script src="../assets/vendors/toastr/toastr.min.js"></script>
     <script src="../assets/vendors/jquery-validation/dist/jquery.validate.min.js"></script>
     <script src="../assets/vendors/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-    <!-- PAGE LEVEL PLUGINS-->
-    <script src="../assets/vendors/chart.js/dist/Chart.min.js"></script>
-    <script src="../assets/vendors/jquery.easy-pie-chart/dist/jquery.easypiechart.min.js"></script>
-    <script src="../assets/vendors/jvectormap/jquery-jvectormap-2.0.3.min.js"></script>
-    <script src="../assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+   
     <!-- CORE SCRIPTS-->
     <script src="../assets/js/app.min.js"></script>
 
