4852c7a96236f6d3d4f5c3d20ae3ddbf2070ba2d
Sudarshan Amirineni <suda_amir@hotmail.com>
2018-03-25 19:55:21 +0530

    Add employee is complted

--- new/admin/adding_emp.php
+++ new/admin/adding_emp.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once('config.php');
-session_start();
-
 
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -17,7 +15,7 @@ $aadhar = $_POST['aadhar'];
 $ecp = $_POST['ecp'];
 $ecm = $_POST['ecm'];
 $blood = $_POST['blood'];
-$qualifications = $_POST['qualifications'];
+$qualifications = implode(",", $_POST['qualifications']);
 
 if (!file_exists('employee/'.$name)) {
     mkdir('employee/'.$name, 0777, true);
@@ -29,27 +27,30 @@ $qual_docs = $target_dir . basename($_FILES["qual_docs"]["name"]);
 
 if($noe == "Employee"){
 	$offer = $target_dir . basename($_FILES["offer_letter"]["name"]);
+	$service_aggr = "";
 }else{
 	$service_aggr = $target_dir . basename($_FILES["service_aggr"]["name"]);	
+	$offer = "";
 }
 $pic = $target_dir . basename($_FILES["pic"]["name"]);
 
 
 $stmt1 = $connection->prepare("SELECT * FROM employee WHERE 1");
 $stmt1->execute();
-$data = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-$id = $data[0]['emp_id'];
-$newstring = substr($id, -3);
-$newstring++;
-
+$data = $stmt1->fetchAll(PDO::FETCH_OBJ);
+foreach ($data as $key) {
+	$id = $key->id;
+}
+$id++;
+$newDate = date("dmY", strtotime($doj));
 if($noe == "Employee"){
-	$emp_id ="EMP".$doj."$newstring";	
+	$emp_id ="EMP".$newDate."$id";	
 }else{
-	$emp_id ="CONT".$doj."$newstring";
+	$emp_id ="CONT".$newDate."$id";
 }
 var_dump($emp_id);
 try{
-	$stmt = $connection->prepare("INSERT INTO `employee`(`emp_id`, `name`, `nat_emp`, `personal_no`, `email`, `dob`, `doj`, `design`, `pan_no`, `aadhar_no`, `perm_addr`, `contct_det`, `emr_cntct_per`, `emr_cntct_no`, `blood_grp`, `qualifications`, `supprt_docs`, `serv_agr`, `offr_letter`, `emp_pic`) VALUES (:emp_id, :name, :noe, contact, :email, :dob, :doj, :designation, :pan, :aadhar, :addr, :contact, :ecp, :ecm, :blood, :qualifications, :qual_docs, :service_aggr, :offer,:pic)");
+	$stmt = $connection->prepare("INSERT INTO `employee`(`emp_id`, `name`, `nat_emp`, `personal_no`, `email`, `dob`, `doj`, `design`, `pan_no`, `aadhar_no`, `perm_addr`, `contct_det`, `emr_cntct_per`, `emr_cntct_no`, `blood_grp`, `qualifications`, `supprt_docs`, `serv_agr`, `offr_letter`, `emp_pic`) VALUES (:id, :name, :noe, :contact, :email, :dob, :doj, :designation, :pan, :aadhar, :addr, :contact, :ecp, :ecm, :blood, :qualifications, :qual_docs, :service_aggr, :offer, :pic)");
 
 	$stmt->bindParam("id", $emp_id,PDO::PARAM_STR) ;
 	$stmt->bindParam("name", $name,PDO::PARAM_STR) ;
@@ -70,7 +71,6 @@ try{
 	$stmt->bindParam("offer", $offer,PDO::PARAM_STR) ;
 	$stmt->bindParam("service_aggr", $service_aggr,PDO::PARAM_STR) ;
 	$stmt->bindParam("pic", $pic,PDO::PARAM_STR) ;
-
 	$stmt->execute();	
 }
 catch(PDOException $ae)
